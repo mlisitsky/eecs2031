0118eb00f3fbb78c977bc4ee3bbd70348dde6359
mlisitsky <mlisitsky@rogers.com>
2018-10-02 11:17:32 -0400

    update

--- lab04/irtester.c
+++ lab04/irtester.c
@@ -6,11 +6,23 @@ int main(int argc, char *argv[])
   int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
+  pinMode (1, OUTPUT);
+  pinMode (2, OUTPUT);
   while(1) {
     printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
+    while(digitalRead(0) == 1) {
+	    digitalWrite (1, HIGH);
+	    delay (300);
+	    digitalWrite (1, LOW);
+	    delay (300);
+    }
     printf("Waiting for event\n");
-    while(digitalRead(0) == 0);
+    while(digitalRead(0) == 0) {
+	    digitalWrite (2, HIGH);
+	    delay (300);
+	    digitalWrite (2, LOW);
+	    delay (300);
+    }
     printf("Alarm\n");
   }
   /*NOTREACHED*/
